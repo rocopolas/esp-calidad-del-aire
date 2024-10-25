b95cbe9bdaa448abbe4c59fc8535c74596a342f9
rocopolas <86751781+rocopolas@users.noreply.github.com>
2024-10-25 11:41:24 -0300

    Add files via upload

--- ESP8266/PAGINA1CODE.h
+++ ESP8266/PAGINA1CODE.h
@@ -1,117 +1,6 @@
 const char PAGINA_PRINCIPAL[] = 
 R"=====( 
-<!DOCTYPE html>
-<html>
-    <style>
-            header {
-            background-color: #08a1c7;
-            position: relative;
-            height: 50px; 
-        }
-
-        header h1 {
-            font-family: monospace;
-            margin: 0;
-            position: absolute;    
-            top: 50%;              
-            left: 50%;             
-            transform: translate(-50%, -50%); 
-        }
-
-        .menu ul {
-            display: flex;
-            padding: 0;
-            margin: 0;
-            list-style: none;
-        }
-
-        .menu a {
-            display: block;
-            padding: 1em;
-            background-color: #08a1c7;
-            text-decoration: none;
-            color: #191C26;
-        }
-
-        .menu a:hover {
-            background-color: #036988;
-        }
-
-        .menu ul li ul {
-            display: none;
-            position: absolute;
-            top: 100%;
-            left: 0;
-            background-color: #08a1c7;
-            padding: 0;
-            margin: 0;
-            list-style: none;
-            width: 100px;
-        }
-
-        .menu ul li ul li a {
-            padding: 1em;
-            background-color: #08a1c7;
-            width: 100%;
-        }
-
-        .menu ul li a:hover + ul, .menu ul li ul:hover {
-            display: block;
-        }
-
-
-        a, p {
-            font-size: large;
-            font-family: monospace;
-        }
-
-        h1 {
-            font-family: monospace;
-        }
-
-        .titulo{
-            margin-left: 10px;
-        }
-
-        canvas{
-            max-width: 700px;
-            max-height: 300px;
-        }
-
-        body{
-            max-width: 50%;
-            margin-left: 25%;
-        }
-    </style>
-
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    
-    <header>
-        <nav class="menu">
-            <ul>
-                <li><a href="#">Menu</a>
-                    <ul>
-                        <li><a href="#1">Inicio</a></li>
-                        <li><a href="#2"">Control</a></li>
-                        <li><a href="#3">Carpeta tecnica</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
-        <h1>ESP CALIDAD DEL AIRE</h1>
-    </header>
-    <body>
-        <h1 href="#1" class="titulo">Inicio</h1>
-        <p>asdasdasdasdsda</p>
-        <h1 href="#2" class="titulo">Control</h1>
-        <p>Temperatura: TEMP01</p>
-        <p>Calidad del aire: CAL01</p>
-        <p>Estado ventiladores: VEN01</p>
-        <div>
-            <canvas id="grafico"></canvas>
-        </div>
-        <script>
-            const ctx = document.getElementById('grafico');
+<!DOCTYPE html><html><style>header{background-color:#08a1c7;position:relative;height:50px}header h1{font-family:monospace;margin:0;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%)}.menu ul{display:flex;padding:0;margin:0;list-style:none}.menu a{display:block;padding:1em;background-color:#08a1c7;text-decoration:none;color:#191c26}.menu a:hover{background-color:#036988}.menu ul li ul{display:none;position:absolute;top:100%;left:0;background-color:#08a1c7;padding:0;margin:0;list-style:none;width:100px}.menu ul li ul li a{padding:1em;background-color:#08a1c7;width:100%}.menu ul li a:hover+ul,.menu ul li ul:hover{display:block}a,p{font-size:large;font-family:monospace}h1{font-family:monospace}.titulo{margin-left:10px}canvas{max-width:700px;max-height:300px}body{max-width:50%;margin-left:25%}</style><script src="https://cdn.jsdelivr.net/npm/chart.js"></script><header><nav class="menu"><ul><li><a href="#">Menu</a><ul><li><a href="#1">Inicio</a></li><li><a href="#2">Control</a></li><li><a href="#3">Carpeta tecnica</a></li></ul></li></ul></nav><h1>ESP CALIDAD DEL AIRE</h1></header><body><h1 href="#1" class="titulo">Inicio</h1><p>asdasdasdasdsda</p><h1 href="#2" class="titulo">Control</h1><p>Temperatura: TEMP01 GRADOS CENTIGRADOS</p><p>Calidad del aire: CAL01 PPM</p><p>Estado ventiladores: VEN01</p><p>Cantidad de humedad: HUM01 %</p><div><canvas id="grafico"></canvas></div><script>const ctx = document.getElementById('grafico');
           
             new Chart(ctx, {
               type: 'scatter',
@@ -129,6 +18,11 @@ R"=====(
                   data: [TEM001, TEM002, TEM003, TEM004, TEM005, TEM006],
                   borderWidth: 1
                 },{
+                  type: 'line',
+                  label: 'Humedad',
+                  data: [HUM001, HUM002, HUM003, HUM004, HUM005, HUM006],
+                  borderWidth: 1
+                },{
                   type: 'bar',
                   label: 'Estado ventiladores',
                   data: [VEN001, VEN002, VEN003, VEN004, VEN005, VEN006],
@@ -142,12 +36,5 @@ R"=====(
                   }
                 }
               }
-            });
-        </script>
-          
-    </body>
-    <footer>
-
-    </footer>
-</html>
+            });</script></body><footer></footer></html>
 )=====";
--- ESP8266/WEBSERVER.ino
+++ ESP8266/WEBSERVER.ino
@@ -1,23 +1,32 @@
-#include <ESP8266WiFi.h> // Incluye la biblioteca WiFi para manejar la conexión a la red.
-#include <ESP8266WebServer.h> // Incluye la biblioteca WebServer para crear un servidor web.
+#include <WiFi.h> // Incluye la biblioteca WiFi para manejar la conexión a la red.
+#include <WebServer.h> // Incluye la biblioteca WebServer para crear un servidor web.
 #include "PAGINA1CODE.h" // Incluye el archivo que contiene el código HTML de la página principal.
+
+#include <DHT.h>		// importa la Librerias DHT
+#include <DHT_U.h>
 
 const char* ssid = "LABO"; // Define el nombre de la red WiFi a la que se conectará.
 const char* password = ""; // Define la contraseña de la red WiFi.
-ESP8266WebServer server(80); // Crea un objeto para el servidor web en el puerto 80.
+WebServer server(80); // Crea un objeto para el servidor web en el puerto 80.
 
-const int CDA = 5; //calidad del aire
+const int CDA = 27; //calidad del aire
 const int VEN = 4; //ventilador
-const int TEM = 0; //temperatura
-int FLAG = 0;
+const int BUZ = 26;
+
+#define DHTPIN 25
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 int calidad_estado;
-int temperatura_estado;
+float temperatura_estado;
 int ventilador_estado;
+float humedad_estado;
 
 int CDA_GRA[6] = {0,0,0,0,0,0}; 
 int VEN_GRA[6] = {0,0,0,0,0,0}; 
 int TEM_GRA[6] = {0,0,0,0,0,0}; 
+int HUM_GRA[6] = {0,0,0,0,0,0}; 
 
 unsigned long previousMillis = 0; 
 const long interval = 5000;
@@ -25,9 +34,11 @@ const long interval = 5000;
 void setup() {
   Serial.begin(115200); // Inicia la comunicación serial a 115200 baudios.
 
-  pinMode(TEM, INPUT); 
+  dht.begin();
+
   pinMode(CDA, INPUT); 
   pinMode(VEN, OUTPUT); 
+  pinMode(BUZ, OUTPUT); 
 
   // Conexión a la red WiFi
   WiFi.begin(ssid, password); // Inicia la conexión a la red WiFi con el SSID y la contraseña especificados.
@@ -47,60 +58,74 @@ void setup() {
 
 void loop() {
   server.handleClient(); // Maneja las solicitudes de los clientes que se conectan al servidor.
+
   calidad_estado = digitalRead(CDA);
-  temperatura_estado = digitalRead(TEM);
+  temperatura_estado = dht.readTemperature();
   ventilador_estado = digitalRead(VEN);
+  humedad_estado = dht.readHumidity();
+
+  if(calidad_estado > 900 || temperatura_estado > 45){
+    digitalWrite(BUZ, HIGH);
+    // digitalWrite(VEN, HIGH);
+  } else {
+    digitalWrite(BUZ, LOW);
+    // digitalWrite(VEN, LOW);
+  }
 
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    
+
     for (int i = 0; i < 5; i++) {
       CDA_GRA[i] = CDA_GRA[i + 1];
       VEN_GRA[i] = VEN_GRA[i + 1];
       TEM_GRA[i] = TEM_GRA[i + 1];
+      HUM_GRA[i] = HUM_GRA[i + 1];
     }
     CDA_GRA[5] = calidad_estado;
     VEN_GRA[5] = ventilador_estado;
     TEM_GRA[5] = temperatura_estado;
+    HUM_GRA[5]=  humedad_estado;
 
   }
 
 }
 
 void handleRoot() {
-  String peticion = server.arg("letra"); // Lee el valor del parámetro "estado" de la URL.
-  if (peticion.equals("S")){
-    FLAG = 1;
-  }
-  else if (peticion.equals("N")){
-    FLAG = 0;
-  }
+  
   String Pagina = PAGINA_PRINCIPAL;
   Pagina.replace("TEMP01",String(temperatura_estado));
   Pagina.replace("CAL01",String(calidad_estado));
   Pagina.replace("VEN01",String(ventilador_estado));
+  Pagina.replace("HUM01",String(humedad_estado));
 
   Pagina.replace("CAL006",String(CDA_GRA[5]));
-    Pagina.replace("CAL005",String(CDA_GRA[4]));
-    Pagina.replace("CAL004",String(CDA_GRA[3]));
-    Pagina.replace("CAL003",String(CDA_GRA[2]));
-    Pagina.replace("CAL002",String(CDA_GRA[1]));
-    Pagina.replace("CAL001",String(CDA_GRA[0]));
+  Pagina.replace("CAL005",String(CDA_GRA[4]));
+  Pagina.replace("CAL004",String(CDA_GRA[3]));
+  Pagina.replace("CAL003",String(CDA_GRA[2]));
+  Pagina.replace("CAL002",String(CDA_GRA[1]));
+  Pagina.replace("CAL001",String(CDA_GRA[0]));
 
-    Pagina.replace("TEM006",String(TEM_GRA[5]));
-    Pagina.replace("TEM005",String(TEM_GRA[4]));
-    Pagina.replace("TEM004",String(TEM_GRA[3]));
-    Pagina.replace("TEM003",String(TEM_GRA[2]));
-    Pagina.replace("TEM002",String(TEM_GRA[1]));
-    Pagina.replace("TEM001",String(TEM_GRA[0]));
+  Pagina.replace("TEM006",String(TEM_GRA[5]));
+  Pagina.replace("TEM005",String(TEM_GRA[4]));
+  Pagina.replace("TEM004",String(TEM_GRA[3]));
+  Pagina.replace("TEM003",String(TEM_GRA[2]));
+  Pagina.replace("TEM002",String(TEM_GRA[1]));
+  Pagina.replace("TEM001",String(TEM_GRA[0]));
 
-    Pagina.replace("VEN006",String(VEN_GRA[5]));
-    Pagina.replace("VEN005",String(VEN_GRA[4]));
-    Pagina.replace("VEN004",String(VEN_GRA[3]));
-    Pagina.replace("VEN003",String(VEN_GRA[2]));
-    Pagina.replace("VEN002",String(VEN_GRA[1]));
-    Pagina.replace("VEN001",String(VEN_GRA[0]));
+  Pagina.replace("VEN006",String(VEN_GRA[5]));
+  Pagina.replace("VEN005",String(VEN_GRA[4]));
+  Pagina.replace("VEN004",String(VEN_GRA[3]));
+  Pagina.replace("VEN003",String(VEN_GRA[2]));
+  Pagina.replace("VEN002",String(VEN_GRA[1]));
+  Pagina.replace("VEN001",String(VEN_GRA[0]));
+
+  Pagina.replace("HUM006",String(HUM_GRA[5]));
+  Pagina.replace("HUM005",String(HUM_GRA[4]));
+  Pagina.replace("HUM004",String(HUM_GRA[3]));
+  Pagina.replace("HUM003",String(HUM_GRA[2]));
+  Pagina.replace("HUM002",String(HUM_GRA[1]));
+  Pagina.replace("HUM001",String(HUM_GRA[0]));
 
   server.send(200, "text/html", Pagina); // Envía la página principal al cliente que hace la solicitud.
 }
